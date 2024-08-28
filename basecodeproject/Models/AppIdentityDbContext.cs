4c60a8e3ae90f9a94e5353a8adb0d87d9497a54a
Coşkun ATAM <coskunatam90@gmail.com>
2024-08-28 13:11:18 +0300

    first commit

---
+++ basecodeproject/Models/AppIdentityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace basecodeproject.Models
+{
+	public class AppIdentityDbContext:IdentityDbContext
+	{
+		public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+		{
+
+		}
+	}
+}
+
