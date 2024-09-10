0016f54756893d796aa9243c150c94c76586cc20
Coşkun ATAM <coskunatam90@gmail.com>
2024-09-10 17:49:42 +0300

    başlama anında kullanıcılar kontrol edildi yok ise eklendi

--- basecodeproject/Program.cs
+++ basecodeproject/Program.cs
@@ -39,9 +39,19 @@ using(var scope =app.Services.CreateScope())
 
     var identitydbcontext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
 
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>;
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
 
     identitydbcontext.Database.Migrate(); //  ile uygulama ayağa kalktığında otomatik olarak update - migration çalıştırıyor
+
+    if (!userManager.Users.Any())
+    {
+        userManager.CreateAsync(new AppUser() { UserName = "user1", Email = "user1@outlook.com", }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user2", Email = "user2@outlook.com", }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user3", Email = "user3@outlook.com", }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user4", Email = "user4@outlook.com", }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser() { UserName = "user5", Email = "user5@outlook.com", }, "Password12*").Wait();
+
+    }
 };
 
 
