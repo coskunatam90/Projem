e691c0d3be97a7b3836de58b0c29087a4f6a1d3e
Coşkun ATAM <coskunatam90@gmail.com>
2024-09-10 18:17:46 +0300

    PasswordSignInAsync FindByEmailAsync işlemleri yapıldı

---
+++ basecodeproject/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using basecodeproject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace basecodeproject.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+ 
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  async Task<IActionResult> Login(string email, string password)
+        {
+
+            var hasUser = await _userManager.FindByEmailAsync(email);
+            if (hasUser == null)
+            {
+                return View();
+            }
+
+            var signInResult = await _signInManager.PasswordSignInAsync(hasUser,password,true,false);
+             
+
+
+
+        }
+
+
+
+    }
+}
+
